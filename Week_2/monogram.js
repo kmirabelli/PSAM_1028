533b4cb3867ce1f3b1aabd1f06df4808615515f1
Katelyn Mirabelli <katelyn.mirabelli@gmail.com>
2018-02-01 21:11:00 -0500

    Create monogram.js

---
+++ Week_2/monogram.js
@@ -0,0 +1,19 @@
+var firstName = "Katelyn"
+
+undefined
+var middleName = "Marie"
+
+undefined
+var lastName = "Mirabelli"
+undefined
+var firstLetter = firstName.slice(0,1)
+
+undefined
+var middleLetter = middleName.slice(0,1)
+
+undefined
+var lastLetter = lastName.slice(0,1)
+
+undefined
+monogram = firstLetter+middleLetter+lastLetter
+"KMM"
